83df9373d31d45a29fa58c27da4e25533b50dfc5
Guli_tp48@itstep.edu.az <Guli_tp48@itstep.edu.az>
2022-10-23 21:53:58 +0400

    practice

--- Ajax/wwwroot/ts/index.ts
+++ Ajax/wwwroot/ts/index.ts
@@ -1,5 +1,67 @@
 import $ from "jquery";
-$(function ()
-{
-    console.log("Hello world");
+
+export interface Rating {
+    Source: string;
+    Value: string;
+}
+
+export interface RootObject {
+    Title: string;
+    Year: string;
+    Rated: string;
+    Released: string;
+    Runtime: string;
+    Genre: string;
+    Director: string;
+    Writer: string;
+    Actors: string;
+    Plot: string;
+    Language: string;
+    Country: string;
+    Awards: string;
+    Poster: string;
+    Ratings: Rating[];
+    Metascore: string;
+    imdbRating: string;
+    imdbVotes: string;
+    imdbID: string;
+    Type: string;
+    DVD: string;
+    BoxOffice: string;
+    Production: string;
+    Website: string;
+    Response: string;
+}
+
+
+$(function () {
+
+    let routes: Map<string, () => void> = new Map<string, () => void>();
+    let $body = $("body");
+    routes.set("/HtmlProjects-/Ajax/index.html", () => {
+        const apiKey: string = "31ed7c20";
+
+        let $title = $("#tytle");
+        let $typeFilm = $("#typeFilm");
+        let $year = $("#year");
+        let $filmData = $("#filmData");
+        let $getFilm = $("#getFilm");
+        $getFilm.on("click", (e) => {
+            let title = $title.val();
+
+            // TODO: Add validation for lat lon
+
+            $.get(`http://www.omdbapi.com/?apikey=${apiKey}&/?t=${title}&`).done((data) => {
+                let response: RootObject = data;
+
+                $filmData.empty();
+                $filmData.append(`<div>Title ${response.Title} Year ${response.Year} </div>`);
+            });
+        });
+        let result = routes.get(window.location.pathname);
+
+        if (result !== undefined) {
+            result();
+        }
+    });
 });
