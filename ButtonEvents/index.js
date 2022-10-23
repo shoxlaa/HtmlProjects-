4f1354ff0fdba457df69b7d7d68b46b6ee93b47d
shoxlaa <shahla.guliyeva02@gmail.com>
2022-10-23 16:59:37 +0400

    End second lesson

---
+++ ButtonEvents/index.js
@@ -0,0 +1,39 @@
+$(function () {
+    $(document.body).css({
+        "font-family": "TAHOMA",
+    });
+
+    let $nodes = document.body.querySelectorAll("#cont1","#cont2");
+
+
+    // let $tip1 = $("#tip1");
+    // let $tip2 = $("#tip2");
+    // $tip1.hide();
+    // $tip2.hide();
+    //
+    // let $showButton = $("#show-nodes-up");
+    // let $showButtonDown = $("#show-nodes-down");
+    //
+    // $showButton.on("mouseover", (e) => {
+    //
+    //     $tip1.slideDown();
+    //
+    // });
+    //
+    //
+    // $showButton.on("mouseout", (e) => {
+    //     $tip1.slideDown();
+    //     $tip1.hide();
+    // });
+    //
+    // $showButtonDown.on("mouseover", (e) => {
+    //
+    //     $tip2.slideDown();
+    //
+    // });
+    //
+    // $showButtonDown.on("mouseout", (e) => {
+    //     $tip2.slideUp();
+    //
+    // });
+});
