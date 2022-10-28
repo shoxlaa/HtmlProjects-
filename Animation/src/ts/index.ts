4583acdfc999191f47da6f0be5ad7d5b75c059a4
shoxlaa <shahla.guliyeva02@gmail.com>
2022-10-28 18:03:47 +0400

    pagination

---
+++ Animation/src/ts/index.ts
@@ -0,0 +1,64 @@
+import $ from "jquery";
+$(function f()
+{
+    let routes: Map<string, () => void> = new Map<string, () => void>();
+    routes.set("/FBES_1213_JS/Animation/pages/index.html", () => {
+        //new MyjQuery("body").addClass("main-page").addClass("second-class");
+        let counter: number = 0;
+
+        // let id: number = setInterval(function () {
+        //    console.log(counter++);
+        //
+        //    if (counter === 10)
+        //    {
+        //        clearInterval(id);
+        //    }
+        // }, 1000);
+        //
+        // let timeoutId: number = setTimeout(function f() {
+        //     console.log("delayed");
+        //     setTimeout(f, 1000);
+        // }, 1000);
+
+        //clearTimeout(timeoutId);
+
+        //console.log(`id: ${id}`);
+        // Animations
+
+        let $animationTarget = $(".ic");
+
+        // $animationTarget.slideUp();
+        //
+        // setTimeout(function ()
+        // {
+        //     $animationTarget.slideDown();
+        // }, 1000);
+        $.easing
+        $animationTarget.animate({
+            width: "200px",
+            height: "25px",
+        },  {
+            duration: 3000,
+            complete()
+            {
+                console.log("First animation complete");
+            }
+        }).animate({
+            marginLeft: "20px",
+            paddingTop: "20px"
+        }, () => {
+            console.log("Second animation complete");
+        });
+
+        $animationTarget.on("click", (e) => {
+            $animationTarget.stop(true);
+        });
+    });
+
+    let result = routes.get(window.location.pathname);
+
+    if (result !== undefined)
+    {
+        result();
+    }
+});
--- MovieApi/src/ts/index.ts
+++ MovieApi/src/ts/index.ts
@@ -26,51 +26,89 @@ $(function f() {
         let $type = $(".movie-type");
         let $title = $(".movie-name");
         let $year = $(".year");
-        let $details = $(".button-details");
+        //let $details = $(".button-details");
 
         let $movieList = $("#movie-list");
-        let $movieTypeSelector =$("#movie-type-selector");
 
+        let $openModel = $(".button-details");
+        let $modal = $("#myModal");
+        let $span = $(".close");
+        let $l = $(".l");
 
         let $d = $("#d");
         let $search = $("#search");
         let $pageBack = $("#back");
-        let $pageNumber=$("#page-number");
-        let $pageButton= $("#page-button");
+        let $pageNumber = $("#page-number");
+        let $pageButton = $("#page-button");
         let $pageNext = $("#next");
         let $movieTitle = $("#movie-title");
-        $search.on("click", (e) => {
-            $.get(`http://www.omdbapi.com/?i=tt3896198&apikey=31ed7c20&s=${$movieTitle.val()}&type${$movieTypeSelector}`).done((data) => {
-                response = data;
-                addItems($movieList,response);
-            });
-        });
-        $pageButton.on("click",(e) => {
-            $.get(`http://www.omdbapi.com/?i=tt3896198&apikey=31ed7c20&s=${$movieTitle.val()}&page=${$pageNumber.val()}`).done((data) => {
-                response = data;
-                addItems($movieList,response);
-            });
+        let $num = $("#numeration");
+        let pageNum = 1;
 
+        $l.on("click", (e) => {
+            console.log("Details");
+        });
+        $openModel.on("click", (e) => {
+            $modal.css("display", "block");
+            console.log("opened");
+        });
+
+        $span.on("click", (e) => {
+            $modal.css("display", "none");
         })
 
+        $search.on("click", (e) => {
+            let $movieTypeSelector = $("#movie-type-selector").val().toString();
+            $.get(`http://www.omdbapi.com/?i=tt3896198&apikey=31ed7c20&s=${$movieTitle.val()}&type=${$movieTypeSelector}`).done((data) => {
+                response = data;
+                addItems($movieList, response);
+            });
+        });
+        $pageButton.on("click", (e) => {
+            let $movieTypeSelector = $("#movie-type-selector").val().toString();
+            $.get(`http://www.omdbapi.com/?i=tt3896198&apikey=31ed7c20&s=${$movieTitle.val()}&page=${$pageNumber.val()}&type=${$movieTypeSelector}`).done((data) => {
+                response = data;
+                addItems($movieList, response);
+            });
+            pageNum = parseInt($pageNumber.val().toString());
+            document.getElementById("numeration").textContent = pageNum.toString();
+        })
+
+        $pageNext.on("click", (e) => {
+            let $movieTypeSelector = $("#movie-type-selector").val().toString();
+            pageNum += 1;
+            $.get(`http://www.omdbapi.com/?i=tt3896198&apikey=31ed7c20&s=${$movieTitle.val()}&page=${pageNum.toString()}&type=${$movieTypeSelector}`).done((data) => {
+                response = data;
+                addItems($movieList, response);
+            });
+        })
+        $pageBack.on("click", (e) => {
+            let $movieTypeSelector = $("#movie-type-selector").val().toString();
+            pageNum -= 1;
+            $.get(`http://www.omdbapi.com/?i=tt3896198&apikey=31ed7c20&s=${$movieTitle.val()}&page=${pageNum.toString()}&type=${$movieTypeSelector}`).done((data) => {
+                response = data;
+                addItems($movieList, response);
+            });
+        })
 
     });
     let result = routes.get(window.location.pathname);
 
     function addItems(buffer: JQuery<HTMLElement>, response: Root) {
-        let movies= response.Search;
+        let movies = response.Search;
         buffer.empty()
         for (let i = 0; i < parseInt(response.totalResults); i++) {
 
             buffer.append(` <div class="box">
                      <div class="cont1">
-                         <input  class="image" type="image" src=${movies[i].Poster}>
+                         <input  class="image" type="image" src=${movies[i].Poster.toString()}>
                      </div>
                      <div class="cont2">
                          <div class="movie-type">${movies[i].Type}</div>
                          <div class="movie-name">${movies[i].Title}</div>
                          <div class="year">${movies[i].Year}</div>
                          <input type="button" class="button-details" value="Details" >
+                       
                      </div>
                  </div>`)
         }
